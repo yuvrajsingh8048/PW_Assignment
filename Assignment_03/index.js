91f433761b660a82a2ce6def8e9be9eb52324e5d
Yuvraj Singh <124923698+yuvrajsingh8048@users.noreply.github.com>
2023-12-24 17:05:37 +0530

    pw

--- Assignment_03/index.js
+++ Assignment_03/index.js
@@ -7,47 +7,47 @@
 
 // **************** Question 03 ************************
 
-// const { isUtf8 } = require('buffer')
-// const fs = require('fs')
+const { isUtf8 } = require('buffer')
+const fs = require('fs')
 
-// fs.readFile('input.txt', function(err, data) {
-//     if(err) {
-//         console.log('Error in reading a file', err)
-//     }
-//     else {
-//         console.log('Sucessfully read a file')
-//         console.log('Data : ' ,data.toString())
-//     }
-// })
+fs.readFile('input.txt', function(err, data) {
+    if(err) {
+        console.log('Error in reading a file', err)
+    }
+    else {
+        console.log('Sucessfully read a file')
+        console.log('Data : ' ,data.toString())
+    }
+})
 
 // ******************** Question 04 *****************
 
-// const appendData = "Node.js is similar in design to, and influenced by, systems like Ruby's Event Machine and Python's Twisted. Node.js takes the event model a bit further. It presents an event loop as a runtime construct instead of as a library. In other systems, there is always a blocking call to start the event-loop."
-// fs.appendFile('input.txt', appendData, 'utf8', function(err, data) {
-//     if(err) {
-//         console.log('Error in appending a File')
-//     }
-//     else {
-//         console.log('File Append Sucessfully')
-//     }
-// })
+const appendData = "Node.js is similar in design to, and influenced by, systems like Ruby's Event Machine and Python's Twisted. Node.js takes the event model a bit further. It presents an event loop as a runtime construct instead of as a library. In other systems, there is always a blocking call to start the event-loop."
+fs.appendFile('input.txt', appendData, 'utf8', function(err, data) {
+    if(err) {
+        console.log('Error in appending a File')
+    }
+    else {
+        console.log('File Append Sucessfully')
+    }
+})
 
 
 // ************************* Question 05 *****************
 
-// fs.unlink('input.txt', function(err) {
-//     if(err) {
-//         console.log('Error in deleting a File')
-//     }
-//     else {
-//         console.log('Successfully Delete a File')
-//     }
-// })
+fs.unlink('input.txt', function(err) {
+    if(err) {
+        console.log('Error in deleting a File')
+    }
+    else {
+        console.log('Successfully Delete a File')
+    }
+})
 
 // ********************* Question 06 ***********************
-// const os = require('os')
-// console.log('Operating system name : ', os.type())
-// console.log('OS Release : ', os.release())
+const os = require('os')
+console.log('Operating system name : ', os.type())
+console.log('OS Release : ', os.release())
 
 // *************** Question 07 ************************
 
@@ -61,3 +61,4 @@ const server = http.createServer((req, res) => {
 })
 server.listen(port)
 console.log(`Server is running at PORT : ${port}`)
+
